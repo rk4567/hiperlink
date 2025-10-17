191edbc7fe10bfad3b7dc7ea1f53bc7363802c72
rockey kumar <krockey046@gmail.com>
2025-10-17 15:31:49 +0530

    ai agent-1

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,12 @@
 
-import { openai, createAgent } from "@inngest/agent-kit";
+import { openai, createAgent, createTool, createNetwork } from "@inngest/agent-kit";
 import { inngest } from "./client";
 // import { success } from "zod";
 import { Sandbox } from '@e2b/code-interpreter'
-import { getSandbox } from "./utils";
+import { getSandbox, lastAssistantTextMessageContent } from "./utils";
+import { z } from "zod";
+import { PROMPT } from "@/prompt";
+// import { stderr } from "process";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
@@ -15,14 +18,138 @@ export const helloWorld = inngest.createFunction(
 
     const codeAgent = createAgent({
       name: "code-agent",
-      system: "You are an expert next.js seveloper. You write readable, maintainable code.You write simple Next.js $ React snippets",
-      model: openai({ model: "gpt-4o" }),
+      description: "An expert coding agent",
+      system: PROMPT,
+      model: openai({
+        model: "gpt-4.1",
+        defaultParameters: {
+          temperature : 0.1,
+        },
+         }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run the commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+
+              try {
+                const sandbox = await getSandbox(sandboxId)
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data;
+                  }
+                });
+                return result.stdout;
+              } catch (e) {
+                console.error(
+                  `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`,
+                );
+                return `command failed: ${e} \nstdout: ${buffers.stdout}\nstderr: ${buffers.stderr}`;
+              }
+            });
+          }
+        }),
+        createTool({
+          name: "CreateOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            files: z.array(
+              z.object({
+                path: z.string(),
+                content: z.string(),
+              }),
+            )
+          }),
+          handler: async(
+            { files },
+            { step, network }
+          ) => {
+            const newFiles = await step?.run("createOrUpdateFiles", async() => {
+              try {
+                const updatedFiles = network.state.data.files || {};
+                const sandbox = await getSandbox(sandboxId);
+                for (const file of files) {
+                  await sandbox.files.write(file.path.content);
+                  updatedFiles[file.path] = file.content;
+                }
+
+                return updatedFiles;
+              } catch (e) {
+                return "Error: " + e;
+              }
+            });
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
+            }
+          }
+        }),
+        createTool({
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          parameters: z.object({
+            files: z.array(z.string()),
+          }),
+          handler: async({ files }, { step }) => {
+            return await step?.run("readFiles", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const contents = [];
+                for (const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({ path: file, content });
+                }
+                return JSON.stringify(contents);
+              } catch (e) {
+                return "Error" + e;
+              }
+            })
+          },
+        })
+        ],
+        lifecycle: {
+          onResponse: async ({ result, network }) => {
+            const lastAssistantMessageText = 
+            lastAssistantTextMessageContent(result)
+
+            if (lastAssistantMessageText && network) {
+              if (lastAssistantMessageText.includes("<task_summary>")) {
+                network.state.data.summary = lastAssistantMessageText
+              }
+            }
+            return result;
+          },
+        }
     });
 
-    const {output} = await codeAgent.run(
-      `Write the following snippet: ${event.data.value}`,
+    const network = createNetwork({
+      name: "coding-agent-network",
+      agents: [codeAgent],
+      maxIter: 15,
+      router: async ({ network }) => {
+        const summary = network.state.data.summary;
 
-    );
+        if (summary) {
+          return;
+        }
+
+        return codeAgent;
+      }
+    })
+
+    const result = await network.run(event.data.value);
+
+      // const {output} = await codeAgent.run(
+      // `Write the following snippet: ${event.data.value}`,
+// 
+    // );
     
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
@@ -33,7 +160,12 @@ export const helloWorld = inngest.createFunction(
 
     // await step.sleep("wait-a-moment", "5s");
     // return { message: `Hello ${event.data.value}!`};
-    return { output, sandboxUrl };
+    return { 
+      url: sandboxUrl,
+      title: "Fragment",
+      files: result.state.data.files,
+      summary: result.state.data.summary,
+    };
     // const sandboxId = await step.run("get-sandbox-id"), async () => {
     // const sandboxId = await Sandbox.create("")}
     
