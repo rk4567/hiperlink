abacc6b3386a6339658a9ae0a88a6370481630da
rockey kumar <krockey046@gmail.com>
2025-10-18 12:28:43 +0530

    feat: add project management functionality with CRUD operations and integrate project ID in messages

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -184,6 +184,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "The agent failed to produce a valid result.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -193,6 +194,7 @@ export const codeAgentFunction = inngest.createFunction(
         
         return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
