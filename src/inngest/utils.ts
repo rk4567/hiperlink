7585b3216b130abef2f701428ae1eb0c3e94aaf0
rockey kumar <krockey046@gmail.com>
2025-11-27 04:13:35 +0530

    fixes some code

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -3,6 +3,7 @@ import { AgentResult, TextMessage, Message } from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxId: string) {
     const sandbox = await Sandbox.connect(sandboxId)
+    await sandbox.setTimeout(15 * 60 * 1000); // 15 minutes
     return sandbox;
 }
 
--- src/modules/projects/ui/components/usage.tsx
+++ src/modules/projects/ui/components/usage.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useMemo } from "react";
 import { CrownIcon } from "lucide-react";
 import { formatDuration, intervalToDuration } from "date-fns";
 
@@ -14,6 +15,21 @@ export const Usage = ({ points, msBeforeNext }: Props) => {
     
     const { has } = useAuth();
     const hasProAccess = has?.({ plan: "pro" });
+
+    const resetTime = useMemo(() => {
+        try {
+            return formatDuration(
+                intervalToDuration({
+                    start: new Date(),
+                    end: new Date(Date.now() + msBeforeNext),
+                }),
+                { format: ["months", "days", "hours"] }
+            )
+        } catch (error) {
+            console.error("Error formatting duration ", error);
+            return "unknown";
+        }
+    },[msBeforeNext]);
 
     return (
         <div className="rounded-t-xl bg-background border border-b-0 p-2.5">
