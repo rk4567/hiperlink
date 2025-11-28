53fe3065fb2be94be05b6a4dfe9fc23e30d07066
rockey kumar <krockey046@gmail.com>
2025-11-28 08:13:44 +0530

    update eslint configuration to ignore generated files in next.config.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: ["**/generated/*"]
-  }
+  ...compat.extends("next/core-web-vitals", "next/typescript")
 ];
 
 export default eslintConfig;
